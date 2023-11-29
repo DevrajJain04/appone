27a041223cb1fe17b094777fbf55c96a6c40843d
Devraj Jain <devrajjain4@gmail.com>
2023-11-29 19:57:34 +0530

    step-11

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,9 @@ import 'package:appone/views/register_view.dart';
 import 'package:appone/views/verifyemail_view.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await AuthService.firebase().initialize();
   runApp(
     MaterialApp(
       title: 'Flutter Demo',
@@ -44,7 +45,7 @@ class HomePage extends StatelessWidget {
                 } else {
                   return const VerifyEmailView();
                 }
-              } else {
+              } else { 
                 return const LoginView();
               }
             default:
